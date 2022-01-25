a3c1fc67bbe70ff922350ef46063270ba5fda9b3
thiagoalmeidadon <thiagodons@gmail.com>
2022-01-25 16:49:14 -0300

    implementando método host

--- parsing.sh
+++ parsing.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 clean(){
     rm *.html
+    echo "Limpando arquivo html"
+    echo -e "\n"
 }
 parsing(){
-    grep "a href" index.html | cut -d '"' -f 2 | grep "http://" > $host.txt
+    grep "a href" index.html | cut -d '"' -f 2 | grep "http://" | cut -d "/" -f 3 > $host.txt
+    echo "Resultados salvos em $host.txt"
+    echo -e "\n"
+}
+hosts(){
+    echo "Resolvendo URLs em: $host"
+    echo " "
+    for h in $(cat $host.txt); do
+    host $h | grep "has address";
+    done
+    echo -e "\n"
 }
 
 if [ "$1" == "" ]
 then
     echo "Exemplo de uso: $0 google.com"
 else
+    echo " "
     host=$1
     wget $host 1>/dev/null 2>/dev/null 
     parsing
+    hosts
     clean
 fi
 
